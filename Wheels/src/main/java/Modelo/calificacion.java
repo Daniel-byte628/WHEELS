6a7623568be29d7f9227ab6e0971afd55f020c7a
Daniel Gutierrez <danigutierritos2817@gmail.com>
2022-09-25 19:55:20 -0500

    calificacion

---
+++ Wheels/src/main/java/Modelo/calificacion.java
@@ -0,0 +1,36 @@
+package Modelo;
+
+public class calificacion {
+
+    private String referencia;
+    private int puntuacion;
+    private String comentario;
+
+
+    //Getters
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    //Setters
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+}
